50bfc0eea085179715b2b749b3c4f182cb2ce001
riicher1128 <richer11288@gmail.com>
2023-11-22 00:31:06 +0800

    first commit

---
+++ src/app/core/components/navber/navber.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navber',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navber.component.html',
+  styleUrl: './navber.component.css'
+})
+export class NavberComponent {
+
+}
